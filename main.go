786e368e2fb251cd2d66de12c3af37f924ec2112
changmink <changmin043@gmail.com>
2021-05-07 21:48:24 +0900

    [NoIssue] API 응답 결과에서 원하는 것만 파싱하기

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type Response struct {
+	Status        string `json:"status"`
+	StatusMessage string `json:"status_message"`
+	Data          Data   `json:"data"`
+}
+
+type Data struct {
+	MovieCount int     `json:"movie_count"`
+	Limit      int     `json:"limit"`
+	PageNumber int     `json:"page_number"`
+	Movies     []Movie `json:"movies"`
+}
+
+type Movie struct {
+	Id    int    `json:"id"`
+	Url   string `json:"url"`
+	Title string `json:"title"`
+	Image string `json:"small_cover_image"`
+}
 
 func main() {
 	url := "https://yts.mx/api/v2/list_movies.json?sort_by=like_count&order_by=desc&limit=5"
@@ -16,8 +37,12 @@ func main() {
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal("body parse Error")
+		log.Fatal("Get Response Error")
 	}
 
-	fmt.Println(string(body))
+	var response Response
+	if err := json.Unmarshal(body, &response); err != nil {
+		log.Fatal("body parse Error")
+	}
+	fmt.Println(response.Data)
 }
